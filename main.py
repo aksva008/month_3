86d39df29ada5b4d005e39482e891ee2bfa2c7bb
aksva008 <akylaiakisheva008@gmail.com>
2025-11-28 19:07:54 +0600

    lesson2

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = 'My first app' #встроенный параметр
+    greeting_text = ft.Text(value='Hello world', color=ft.Colors.RED)
+    
+    greeting_text.value = 'Hi'
+    greeting_text.color = ft.Colors.PINK_50
+
+
+    page.add(greeting_text)
+
+ft.app(target=main)global
